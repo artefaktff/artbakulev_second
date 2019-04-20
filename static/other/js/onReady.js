66d5580d87d669a85dc6969af77a025e94502d93
a.bakulev <i@artbakulev.com>
2019-04-21 00:49:44 +0300

    Change other/olimp frontend

--- static/other/js/onReady.js
+++ static/other/js/onReady.js
@@ -1,17 +1,19 @@
-$(document).ready(function(){
-   let jumbotron_text_block =  $('#jumbotron-text-block'),
-       h1 = jumbotron_text_block.find(".title")[0],
-       max_width = getComputedStyle(h1).width;
-   jumbotron_text_block.css('max-width', max_width);
+$(document).ready(function () {
+    let jumbotron_text_block = $('#jumbotron-text-block'),
+        h1 = jumbotron_text_block.find(".title")[0],
+        max_width = getComputedStyle(h1).width;
+    jumbotron_text_block.css('max-width', max_width);
 
-   $(document).on('scroll', function(e){
-       let navbar = $('#navbar');
-       if (window.pageYOffset > $('#jumbotron').innerHeight()){
-           navbar.fadeIn();
-       }
-       else if (navbar.css('display') === 'block'){
-           navbar.fadeOut();
-       }
-   });
+    $(document).on('scroll', function (e) {
+        let navbar = $('#navbar'),
+            jumbotron_footer = $('#jumbotron-footer'),
+            height_jumbotron = $('#jumbotron').innerHeight(),
+            height_jumbotron_footer_plus_size = jumbotron_footer.offset().top - jumbotron_footer.height();
+        if (height_jumbotron < window.pageYOffset && window.pageYOffset < height_jumbotron_footer_plus_size) {
+            navbar.fadeIn();
+        } else {
+            navbar.fadeOut();
+        }
     });
+});
 
--- static/other/js/scroll.js
+++ static/other/js/scroll.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
     $(".anchor[href*=#]").on("click", function (e) {
         var anchor = $(this);
         $('html, body').stop().animate({
-            scrollTop: $(anchor.attr('href')).offset().top - 75
+            scrollTop: $(anchor.attr('href')).offset().top
         }, 777);
         e.preventDefault();
         return false;
