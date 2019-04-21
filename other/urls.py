1d0312beaf3b2862b679575acddae753e8089fad
a.bakulev <i@artbakulev.com>
2019-04-21 18:46:59 +0300

    Upload other/internet-tech

--- other/urls.py
+++ other/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 from other import views
 
 urlpatterns = [
+    path('internet-tech/trafic-analyze', views.trafic_analyze),
+    path('internet-tech/neural-networks', views.neural_networks),
+    path('internet-tech', views.internet_tech),
     path('olimp', views.olimp),
 ]
--- other/views.py
+++ other/views.py
@@ -4,3 +4,15 @@ from django.shortcuts import render
 # Create your views here.
 def olimp(request):
     return render(request, "other/index.html")
+
+
+def internet_tech(request):
+    return render(request, "other/internet-tech.html")
+
+
+def trafic_analyze(request):
+    return render(request, "other/net-trafic.html")
+
+
+def neural_networks(request):
+    return render(request, "other/neural-networks.html")
